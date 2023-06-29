661b702e5934da1520e84498e610c634a6984b19
Filip <137682962+Filip-Povidernyi@users.noreply.github.com>
2023-06-29 07:56:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,9 @@ from setuptools import setup, find_namespace_packages
 setup(name='home_work',
       version='1',
       description='sort files in folders code',
-      url='https://github.com/Filip-Povidernyi/go-it_hw',
       author='Filip Povidernyi',
       author_email='p.poviderniy@gmail.com',
       license='MIT',
-      packages=['home_work'],
       packages=find_namespace_packages(),
       install_requires=[],
       entry_points={'console_scripts': ['homework = home_work.HW1:homework']}
